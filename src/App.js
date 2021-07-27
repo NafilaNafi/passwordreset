da5e838ef62833d8a623337e18f61d3f9f4428ac
NafilaNafi <Nafila@outlook.com>
2021-07-27 19:49:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Loginpage from "./loginpage";
+import Reset from "./reset";
+import Forgot from "./forgot";
+import Signup from "./signup";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"; 
+import Dashbord from './dashbord';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+    <div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Loginpage} exact={true}></Route>
+        <Route path="/forgot" component={Forgot} exact={true}></Route>
+        <Route path="/signup" component={Signup} exact={true}></Route>
+        <Route path="/dashboard" component={Dashbord} exact={true}></Route>
+        <Route path="/resetpwd/:token" component={Reset} exact={true}></Route>
+      </Switch>
+    </Router>
     </div>
-  );
+  </>
 }
 
 export default App;
